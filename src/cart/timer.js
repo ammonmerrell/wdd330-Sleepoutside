289e0099fb378c8c402134712f03efaca337b6ac
ammonmerrell <amerrell@byupathway.edu>
2025-11-05 10:15:20 -0700

    finished timer with help of example code

--- src/cart/timer.js
+++ src/cart/timer.js
@@ -9,9 +9,16 @@ let down = 10;
 countd.textContent = `${down}`;
 
 timer.addEventListener("click", () => {
-    setInterval(1);
-    down = down - 1;
-    countd.textContent = `${down}`;
+    setInterval(() => {
+        if (down >= 0) {
+            countd.textContent = down;
+            down = down -1;
+        } else {
+            setTimeout(0)
+            countd.textContent = "Time's up!";
+        }  
+    }, 1000);
+    
 });
 
 
